ac61459074fcbc150b3a52ca8d504746f91ea73c
“Jonathan <jonathan.valere@gmail.com>
2023-12-08 14:16:38 -0300

    insertion mailgun

--- back/index.js
+++ back/index.js
@@ -8,6 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+/* MAILGUN CONFIGURATION */
+const mailgun = new Mailgun(formData);
+const client = mailgun.client({
+  username: "Jonathan VALERE",
+  key: process.env.API_KEY_MAILGUN,
+});
+
 app.get("/", (req, res) => {
   try {
     res.status(200).json({ message: "Welcome" });
@@ -18,10 +25,25 @@ app.get("/", (req, res) => {
 
 app.post("/form", async (req, res) => {
   try {
-    // const { firstname, lastname, email, message } = req.body;
-    console.log(req.body);
-    res.status(200).json({ message: "envoyé" });
+    // console.log(req.body);
+    const { firstname, lastname, email, message } = req.body;
+
+    const messageData = {
+      from: `${firstname} ${lastname} <${email}>`,
+      to: process.env.MAIL_TO,
+      subject: `Formulaire JS - Trip`,
+      text: message,
+    };
+
+    const response = await client.messages.create(
+      process.env.DOMAINE_NAME_MAILGUN,
+      messageData
+    );
+
+    console.log(response);
+    res.status(200).json(response);
   } catch (error) {
+    console.log("dans la partie catch");
     res.status(500).json({ message: error.message });
   }
 });
